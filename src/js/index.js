2a02d6cce26a66cf667a4033453237c03e49cc29
Carolina Uribe Botero <uribebotero.carolin@gmail.com>
2022-08-07 12:05:56 -0500

    Paginas de productos

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+// Start Menu
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+
+  if (navMenu.classList.contains("nav-menu_visible")) {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    navToggle.setAttribute("aria-label", "Abrir menú");
+  }
+});
+// Finish Menu
+
+// Start Img
+
+// Finish Img
